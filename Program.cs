aa18a10f895691af4500e9cb238f5630646519a6
jywkim <jkim589@gmail.com>
2018-08-28 23:49:31 -0400

    modify list contents
    add 2 more names and remove 1
    reference individual items by index

--- Program.cs
+++ Program.cs
@@ -12,6 +12,22 @@ namespace list_collections
             {
                 Console.WriteLine($"Hello {name.ToUpper()}!");
             }
+
+            Console.WriteLine();
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Remove("Ana");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"My name is {names[0]}");
+            Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+
+            Console.WriteLine();
+            Console.WriteLine($"The list has {names.Count} people in it");
         }
     }
 }
