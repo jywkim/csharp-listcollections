0da080b0855a0df337ebee4be8850588b833038c
jywkim <jkim589@gmail.com>
2018-08-29 21:42:09 -0400

    fibonacci sequence first 20 values

--- Program.cs
+++ Program.cs
@@ -9,11 +9,13 @@ namespace list_collections
         {
             // WorkingWithStrings();
             var fibonacciNumbers = new List<int> {1, 1};
-            var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
-            var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+            while (fibonacciNumbers.Count < 20)
+            {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
 
-            fibonacciNumbers.Add(previous + previous2);
-
+                fibonacciNumbers.Add(previous + previous2);
+            }
             foreach(var item in fibonacciNumbers)
                 Console.WriteLine(item);
         }
